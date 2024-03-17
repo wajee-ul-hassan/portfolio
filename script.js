6f4a0107aa5712e4df64dde9f23548efefb09918
wajee-ul-hassan <wajeex001@gmail.com>
2024-03-17 15:23:03 +0500

    done

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function downloadFile() {
     // Replace 'filename.ext' with the name of your file
     var filename = 'cv.txt';
     // If your file is served by the live server, you can use a relative path
-    var fileUrl = '/files/' + filename;
+    var fileUrl = filename;
 
     var link = document.createElement('a');
     link.href = fileUrl;
